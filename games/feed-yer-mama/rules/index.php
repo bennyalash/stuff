4613f0c74c2f48b0c6c5435bc2fd144117a4bafe
bennyalash <108185043+bennyalash@users.noreply.github.com>
2024-01-27 13:22:20 -0500

    Create index.php

---
+++ games/feed-yer-mama/rules/index.php
@@ -0,0 +1,20 @@
+<?php
+
+?>
+
+<html>
+  <body>
+    <div class="rules-content">
+      <h1>Feed 'Yer Mama</h1>
+      <div class="rules-section">
+        <h2>Game Contents</h2>
+        <div class="rules-item">
+          <h3>55 Cards</h3>
+          <div class="sub-item">
+            <p>1 Rules Card</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
