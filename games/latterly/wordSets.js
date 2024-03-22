83e075282ece20c368f42898ce2138ae2fbdb3e4
bennyalash <108185043+bennyalash@users.noreply.github.com>
2024-03-22 09:53:18 -0400

    Create wordSets.js

---
+++ games/latterly/wordSets.js
@@ -0,0 +1,8 @@
+const wordSets = [
+    { startWord: 'BEAN', endWord: 'CART' },     // January 1
+    { startWord: 'JUMP', endWord: 'FROG' },     // January 2
+    { startWord: 'SLEEP', endWord: 'DREAM' },   // December 30
+    { startWord: 'SNOW', endWord: 'IGLOO' }     // December 31
+];
+
+module.exports = wordSets;
