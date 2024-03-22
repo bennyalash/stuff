4bdf186ca1025ae70fa48f50789d9d5b287d4c9a
bennyalash <108185043+bennyalash@users.noreply.github.com>
2024-03-22 10:11:43 -0400

    Update script.js

--- games/latterly/script.js
+++ games/latterly/script.js
@@ -1,6 +1,6 @@
 const wordSets = [
     { startWord: 'BEAN', endWord: 'CART' },     // January 1
-    { startWord: 'JUMP', endWord: 'FROG' },     // January 2
+    { startWord: 'LEAP', endWord: 'FROG' },     // January 2
     { startWord: 'SLEEP', endWord: 'DREAM' },   // December 30
     { startWord: 'SNOW', endWord: 'IGLOO' }     // December 31
 ];
